6587073cccfdb11f0afe5175a963895bb0971823
Cao Qian <talexcao@gmail.com>
2018-03-02 15:50:57 -0800

    Initial Commit

---
+++ Prototype/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Prototype
+//
+//  Created by Cao Qian on 2/3/18.
+//  Copyright © 2018 Cao Qian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
